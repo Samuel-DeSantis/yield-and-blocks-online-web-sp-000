72af7c0e8311c673268361cd020769179274b02f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-22 01:35:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,10 @@
-def hello_t
+def hello_t(array)
 
+  if block_given?
+    array.each { |element|
+      puts element
+    }
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
